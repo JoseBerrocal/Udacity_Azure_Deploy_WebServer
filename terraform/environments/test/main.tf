104353201bd49d7d40516f4e61723db0759964bf
Jose Armando Berrocal Abensur <casita@localhost.localdomain>
2020-07-21 17:31:06 -0500

    feat: Adding the resource group

---
+++ terraform/environments/test/main.tf
@@ -0,0 +1,38 @@
+provider "azurerm" {
+  tenant_id       = "${var.tenant_id}"
+  subscription_id = "${var.subscription_id}"
+  client_id       = "${var.client_id}"
+  client_secret   = "${var.client_secret}"
+  features {}
+}
+terraform {
+  backend "azurerm" {
+    storage_account_name = "tstate32610"
+    container_name       = "tstate"
+    key                  = "vXUTpKk4zaTrGhirLY8oKym3tASyz8xw7i7W4U4uKyZQk9RsiLyf88xNB7dbUhenS86MRWahXxdpSBeUSwycKw=="
+    access_key           = "vXUTpKk4zaTrGhirLY8oKym3tASyz8xw7i7W4U4uKyZQk9RsiLyf88xNB7dbUhenS86MRWahXxdpSBeUSwycKw=="
+  }
+}
+
+locals {
+  tags = {
+    tier        = "${var.tier}"
+    deployment  = "${var.deployment}"
+  }
+}
+
+module "resource_group" {
+  source               = "../../modules/resource_group"
+  resource_group       = "${var.resource_group}"
+  location             = "${var.location}"
+}
+# Reference the AppService Module here.
+
+/*module "app_service" {
+  source               = "../../modules/appservice"
+  location             = "${var.location}"
+  application_type     = "${var.application_type}"
+  resource_type        = "AppService"
+  resource_group       = "${module.resource_group.resource_group_name}"
+  tags                 = "${local.tags}"
+}*/
---
+++ terraform/environments/test/terraform.tfvars
@@ -0,0 +1,15 @@
+# Azure subscription vars
+subscription_id = "010976de-a175-413a-84be-6f56f4caec91"
+client_id = "bdef5e95-4688-4031-83e6-10a337bb9b59"
+client_secret = "/S^dl2@%f6Fo5|)GmUkh8+Huf=\"OUx:'"
+tenant_id = "ff1217c8-dc0a-4f38-ba01-96beee1ab324"
+
+# Resource Group/Location
+location = "West US 2"
+resource_group = "rg_deploy_webpage"
+#resource_type = "AppService"
+application_type = "AzureJBmyApplication1" # This name has to be globally unique.
+
+# Tags
+tier = "Test"
+deployment = "Terraform"
