92d2d3636ee0e9e0e482e6be250d8828414d7637
Jose Armando Berrocal Abensur <casita@localhost.localdomain>
2020-07-29 21:29:05 -0500

    docs: Update default values in variables

--- terraform/environments/test/input.tf
+++ terraform/environments/test/input.tf
@@ -1,48 +1,51 @@
 # Azure GUIDS
-variable "subscription_id" {}
-variable "client_id" {}
-variable "client_secret" {}
-variable "tenant_id" {}
+variable "subscription_id" {default = ""}
+variable "client_id" {default = ""}
+variable "client_secret" {default = ""}
+variable "tenant_id" {default = ""}
 
 # Resource Group/Location
-variable "location" {}
-variable "resource_group" {}
-variable "application_type" {}
+variable "location" {default = ""}
+variable "resource_group" {default = ""}
+variable "application_type" {default = ""}
 
 # Virtual Network
-variable "virtual_network" {}
+variable "virtual_network" {default = ""}
 variable "address_space" {type = list(string)}
 
 #Virtual SubNet
-variable "virtual_subnet" {}
-variable "address_prefix" {}
+variable "virtual_subnet" {default = ""}
+variable "address_prefix" {default = ""}
 
 
 # Network Security Group 
-variable "network_security_group" {}
+variable "network_security_group" {default = ""}
 #variable "resource_group_name" {}
 
 # Network Interface
-variable "network_interface" {}
-variable "ip_configuration_name" {}
+variable "network_interface" {default = ""}
+variable "ip_configuration_name" {default = ""}
 
 # Public IP
-variable "public_ip_name" {}
+variable "public_ip_name" {default = ""}
 
 # Load Balancer
-variable "load_balancer" {}
-variable "frontend_ip_name" {}
+variable "load_balancer" {default = ""}
+variable "frontend_ip_name" {default = ""}
 #variable "network_interface_id" {type    = list(string)}
 
 # Avaliability Set & Virtual Machines
-variable "availability_set" {}
-variable "vm_size" {}
-variable "admin_username" {}
-variable "admin_password" {}
-variable "image_id" {}
-variable "tag_name" {}
+variable "availability_set" {default = ""}
+variable "vm_size" {default = ""}
+variable "admin_username" {default = ""}
+variable "admin_password" {default = ""}
+variable "image_id" {default = ""}
+variable "tag_name" {default = ""}
 
-variable "instance_count" {type = number}
+variable "instance_count" {
+    type = number
+    default = 2 
+}
 
 
 # Tags
